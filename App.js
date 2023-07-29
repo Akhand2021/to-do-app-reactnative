c30e6dc9adbc600eb4a6e068ba155055702fa1e3
Akhand2021 <apsraghuvanshi85@gmail.com>
2023-07-29 13:28:36 +0530

    commit

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { Homepage } from "./src/screen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontLoaded] = useFonts({
+    Montserrat_semibold: require("./assets/fonts/Montserrat-SemiBold.ttf"),
+  });
+  if (!fontLoaded) {
+    return <View></View>;
+  } else {
+    return <Homepage />;
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
